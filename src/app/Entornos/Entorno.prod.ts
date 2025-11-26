1edc0b01b0b252b828e2a32d04ca4e06f6e984ba
prosystem-pro <prosystem490@gmail.com>
2025-11-26 11:44:05 -0600

    Despliegue de carrito web en .net

---
+++ src/app/Entornos/Entorno.prod.ts
@@ -0,0 +1,6 @@
+export const Entorno = {
+  Produccion: true,
+  ApiUrl: 'http://prosystempro-001-site2.qtempurl.com/api/',
+  // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
+  NombreEmpresa: 'PromesaDeDios'
+};
