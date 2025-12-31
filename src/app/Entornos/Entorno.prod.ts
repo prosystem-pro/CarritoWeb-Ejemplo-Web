abc1ed182ee5cb548758b6051dc1ae3a53ba6a34
prosystem-pro <prosystem490@gmail.com>
2025-12-30 23:21:26 -0600

    CAMBIOS

--- src/app/Entornos/Entorno.prod.ts
+++ src/app/Entornos/Entorno.prod.ts
@@ -1,7 +1,7 @@
 export const Entorno = {
   Produccion: true,
-  // ApiUrl: 'https://prosystempro-001-site2.qtempurl.com/api/',
-  ApiUrl: 'https://localhost:7121/api/',
+  ApiUrl: 'https://prosystempro-001-site2.qtempurl.com/api/',
+  // ApiUrl: 'https://localhost:7121/api/',
   // ApiUrl: 'https://carritoweb-promesadios-api-production.up.railway.app/api/',
   NombreEmpresa: 'Ejemplo'
 };
